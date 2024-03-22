bd5572095086bf1f7c29c3e900c6fe5b23611a4d
Arman Mishra <44322824+Arman-Mishra@users.noreply.github.com>
2024-03-22 12:39:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import React, { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  clickHandler = (e) => {
+    if (e.target.innerText === "Increment") {
+      setCount((prev) => prev + 1);
+    } else if (e.target.innerText === "Decrement") {
+      setCount((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={clickHandler}>Increment</button>
+      <button onClick={clickHandler} style={{ marginLeft: "10px" }}>
+        Decrement
+      </button>
+    </div>
+  );
+}
